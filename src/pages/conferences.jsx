1916077694974fb55666dff4887c2e7d2be2d888
Omar Bahareth <omar@omar.engineer>
2018-10-11 11:41:55 +0300

    Format code

--- src/pages/conferences.jsx
+++ src/pages/conferences.jsx
@@ -4,48 +4,50 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Conference from '../components/conference'
 
-let parseConferences = (data) => {
+let parseConferences = data => {
   // TODO: Make this safer
-  return data.allFile.edges[0].node.childConferencesJson.conferences;
+  return data.allFile.edges[0].node.childConferencesJson.conferences
 }
 
-let Conferences = ({data}) => {
-    let conferences = parseConferences(data);
+let Conferences = ({ data }) => {
+  let conferences = parseConferences(data)
 
-    return (
-      <Layout>
-        <div className="container mx-auto py-8">
-          {conferences.map(conference => (
-            <Conference conference={conference} key={conference.name} />
-          ))}
-          <div className="text-center py-4">
-            <Link to="/">Go back to the homepage</Link>
-          </div>
+  return (
+    <Layout>
+      <div className="container mx-auto py-8">
+        {conferences.map(conference => (
+          <Conference conference={conference} key={conference.name} />
+        ))}
+        <div className="text-center py-4">
+          <Link to="/">Go back to the homepage</Link>
         </div>
-      </Layout>
-    )
+      </div>
+    </Layout>
+  )
 }
 
 export default Conferences
 
 // TODO: Use current locale
 export const query = graphql`
-{
-  allFile(filter: {name: {eq: "en"}, relativeDirectory: {eq: "conferences"}}) {
-    edges {
-      node {
-        childConferencesJson {
-          conferences {
-            name
-            date
-            location
-            description
-            website
-            twitter
+  {
+    allFile(
+      filter: { name: { eq: "en" }, relativeDirectory: { eq: "conferences" } }
+    ) {
+      edges {
+        node {
+          childConferencesJson {
+            conferences {
+              name
+              date
+              location
+              description
+              website
+              twitter
+            }
           }
         }
       }
     }
   }
-}
 `
--- src/pages/meetups.jsx
+++ src/pages/meetups.jsx
@@ -4,45 +4,47 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Meetup from '../components/meetup'
 
-let parseMeetups = (data) => {
+let parseMeetups = data => {
   // TODO: Make this safer
-  return data.allFile.edges[0].node.childMeetupsJson.meetups;
+  return data.allFile.edges[0].node.childMeetupsJson.meetups
 }
 
-let Meetups = ({data}) => {
-  let meetups = parseMeetups(data);
-    return (
-      <Layout>
-        <div className="container mx-auto py-8">
-          {meetups.map(meetup => (
-            <Meetup meetup={meetup} key={meetup.name} />
-          ))}
-          <div className="text-center py-4">
-            <Link to="/">Go back to the homepage</Link>
-          </div>
+let Meetups = ({ data }) => {
+  let meetups = parseMeetups(data)
+  return (
+    <Layout>
+      <div className="container mx-auto py-8">
+        {meetups.map(meetup => (
+          <Meetup meetup={meetup} key={meetup.name} />
+        ))}
+        <div className="text-center py-4">
+          <Link to="/">Go back to the homepage</Link>
         </div>
-      </Layout>
-    )
+      </div>
+    </Layout>
+  )
 }
 
 export default Meetups
 
 // TODO: Use current locale
 export const query = graphql`
-{
-  allFile(filter: {name: {eq: "en"}, relativeDirectory: {eq: "meetups"}}) {
-    edges {
-      node {
-        childMeetupsJson {
-          meetups {
-            name
-            description
-            website
-            twitter
+  {
+    allFile(
+      filter: { name: { eq: "en" }, relativeDirectory: { eq: "meetups" } }
+    ) {
+      edges {
+        node {
+          childMeetupsJson {
+            meetups {
+              name
+              description
+              website
+              twitter
+            }
           }
         }
       }
     }
   }
-}
 `
