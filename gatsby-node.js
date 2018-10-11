6d6be1a7fa8dfcef8b9f976d0041e31738e232ee
Omar Bahareth <omar@omar.engineer>
2018-10-11 18:15:38 +0300

    Setup localization using react-i18next

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+const fs = require("fs-extra")
+const path = require("path")
+
+exports.onPostBuild = () => {
+  console.log("Copying locales")
+  fs.copySync(
+    path.join(__dirname, "/src/locales"),
+    path.join(__dirname, "/public/locales")
+  )
+}
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { I18nextProvider } from "react-i18next";
+
+import i18n from "./i18n";
 
 import './layout.css'
 
@@ -17,7 +20,7 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
+      <I18nextProvider i18n={i18n}>
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -30,7 +33,7 @@ const Layout = ({ children }) => (
         <div className="main h-screen border-purple-dark border-t-4">
           {children}
         </div>
-      </>
+      </I18nextProvider>
     )}
   />
 )
