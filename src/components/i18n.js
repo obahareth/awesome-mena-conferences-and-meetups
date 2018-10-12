565abcc95d4e50e07420b43ab15d128efb3012fd
Omar Bahareth <omar@omar.engineer>
2018-10-12 21:14:44 +0300

    Only use pathPrefix for i18n if it exists and is valid

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -3,32 +3,36 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-xhr-backend'
 import { reactI18nextModule } from 'react-i18next'
 
-import config from "../../gatsby-config";
+import config from '../../gatsby-config'
+
+let i18nConfig = {
+  fallbackLng: 'ar',
+
+  // have a common namespace used around the full app
+  ns: ['translations'],
+  defaultNS: 'translations',
+
+  debug: true,
+
+  interpolation: {
+    escapeValue: false, // not needed for react!!
+  },
+
+  react: {
+    wait: true,
+  },
+}
+
+if (config.pathPrefix && config.pathPrefix.length > 0) {
+  i18nConfig.backend = {
+    loadPath: `${config.pathPrefix}/locales/{{lng}}/{{ns}}.json`,
+  }
+}
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(reactI18nextModule)
-  .init({
-    fallbackLng: 'ar',
-
-    // have a common namespace used around the full app
-    ns: ['translations'],
-    defaultNS: 'translations',
-
-    debug: true,
-
-    interpolation: {
-      escapeValue: false, // not needed for react!!
-    },
-
-    react: {
-      wait: true,
-    },
-
-    backend: {
-      loadPath : `${config.pathPrefix}/locales/{{lng}}/{{ns}}.json`
-    },
-  })
+  .init(i18nConfig)
 
 export default i18n
