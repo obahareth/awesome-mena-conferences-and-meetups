886626b20d19c7623d76405dc6624bb4987c8d42
Omar Bahareth <omar@omar.engineer>
2018-10-13 15:25:07 +0300

    Replace sign up for updates with top-right GitHub corner icon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ import Conferences from '../components/conferences'
 import Meetups from '../components/meetups'
 import Tabs from '../components/tabs'
 
-import GithubIcon from '!svg-react-loader!../images/svg-icons/github.svg?name=GithubIcon'
+import GithubCornerRightIcon from '!svg-react-loader!../images/svg-icons/github-corner-right.svg?name=GithubCornerRightIcon'
 import GlobeIcon from '!svg-react-loader!../images/svg-icons/globe.svg?name=GlobeIcon'
 
 let parseConferences = data => {
@@ -28,6 +28,7 @@ const IndexPage = ({ t, data }) => {
   return (
     <Layout>
       <Helmet title={t('heading')} />
+      <GithubCornerRightIcon className="mt-4"/>
       <div className="container mx-auto py-24 px-16">
         <div className="flex items-center">
           <GlobeIcon className="w-8 h-8 fill-current text-purple-dark" />
@@ -46,18 +47,6 @@ const IndexPage = ({ t, data }) => {
             <Meetups meetups={meetups} />
           </div>
         </Tabs>
-        <div className="text-grey-darker font-thin">
-          {t('sign_up_for_updates')}
-        </div>
-        <div className="py-4 flex item-center pb-2">
-          <input className="bg-white shadow-lg p-2 rounded w-1/4" type="text" />
-          <button className="bg-purple-dark text-white p-2 shadow-lg text-sm px-2 ml-2 rounded">
-            {t('submit')}
-          </button>
-        </div>
-        <a href="https://github.com/obahareth/awesome-mena-conferences-and-meetups">
-          <GithubIcon className="w-8 h-6 fill-current text-purple-dark mt-4" />
-        </a>
       </div>
     </Layout>
   )
