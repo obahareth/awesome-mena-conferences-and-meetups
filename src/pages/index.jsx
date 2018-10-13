a02d0ffdad05b08e4592d1bb83d7986df2c27521
Ehsan <ehsan@tamhub.com>
2018-10-13 20:17:05 +0300

    Auto-remove semi-colon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { withNamespaces } from 'react-i18next'
 
 class RedirectIndex extends React.PureComponent {
   componentDidMount() {
-    const homeUrl = withPrefix('/ar/');
+    const homeUrl = withPrefix('/ar/')
     this.props.i18n.changeLanguage('ar')
     navigate(homeUrl)
   }
