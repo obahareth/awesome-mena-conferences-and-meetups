a4844a9e90457b47fca313a6ef6003af6787439d
Omar Bahareth <omar@omar.engineer>
2018-10-13 20:01:55 +0300

    Redirect in componentDidMount instead of constructor
    react-dom's ready function is undefined in production, I'm trying this to possibly fix that.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,10 @@ import { navigate, withPrefix } from 'gatsby'
 import { withNamespaces } from 'react-i18next'
 
 class RedirectIndex extends React.PureComponent {
-  constructor(args) {
-    super(args)
-
-    // Skip build, Browsers only
-    if (typeof window !== 'undefined') {
-      const homeUrl = withPrefix('/ar/');
-      this.props.i18n.changeLanguage('ar')
-      navigate(homeUrl)
-    }
+  componentDidMount() {
+    const homeUrl = withPrefix('/ar/');
+    this.props.i18n.changeLanguage('ar')
+    navigate(homeUrl)
   }
 
   render() {
